9057f12410ebc17f86d5d7a59707d1ad91989c7f
Rien Boonstoppel <47529957+rienboonstoppel@users.noreply.github.com>
2021-12-10 15:21:26 +0100

    Progress 10-12

--- trainer_org.py
+++ trainer_org.py
@@ -12,6 +12,14 @@ from edgeloss import edge_loss2
 import argparse
 
 class LitTrainer(pl.LightningModule):
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("LitModel")
+        parser.add_argument('--learning_rate', type=float, default=1e-2)
+        # parser.add_argument('--std', type=float, default=-.3548)
+        return parent_parser
+
     def __init__(self,
                  netG,
                  lr: float = 0.0002,
@@ -30,7 +38,7 @@ class LitTrainer(pl.LightningModule):
         imgs_lr = torch.clamp((imgs_lr[:10]*self.hparams.std), 0, 1).squeeze()
         imgs_hr = torch.clamp((imgs_hr[:10]*self.hparams.std), 0, 1).squeeze()
         gen_hr = torch.clamp((gen_hr[:10]*self.hparams.std), 0, 1).squeeze()
-        diff = imgs_hr-gen_hr + .5
+        diff = (imgs_hr-gen_hr)*2 + .5
 
         img_grid = torch.cat([torch.stack([a, b, c, d]) for a, b, c, d in zip(imgs_lr, imgs_hr, gen_hr, diff)]).unsqueeze(1)
 
@@ -75,12 +83,21 @@ class LitTrainer(pl.LightningModule):
             loss_edge = self.criterion_edge(gen_hr, imgs_hr)
             loss_pixel = self.criterion_pixel(gen_hr, imgs_hr)
             g_loss = 0.3 * loss_edge + 0.7 * loss_pixel
-
             self.log('Epoch loss/generator', {'Val': g_loss}, on_step=False, on_epoch=True, sync_dist=True)
-            self.log('val_loss', g_loss)
+
+            # self.log('val_loss', g_loss)
+
         if batch_idx % 50 == 0:
             grid = self.make_grid(imgs_lr, imgs_hr, gen_hr)
             self.logger.experiment.add_image('generated images/val', grid, batch_idx*(self.current_epoch+1), dataformats='CHW')
+
+    # def training_epoch_end(self, outputs):
+    #     print(outputs)
+
+    # def validation_epoch_end(self, outputs):
+    #     avg_loss = torch.stack(
+    #         [x['val_loss'] for x in outputs]).mean()
+    #     self.log('val_loss', avg_loss)
 
     def configure_optimizers(self):
         lr = self.hparams.lr
